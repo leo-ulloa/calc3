dcfd0c42d15458cdb611d0f0f336934114ecab38
leo-ulloa <lfu3@njit.edu>
2021-11-05 00:46:18 -0400

    Added history methods, split add, subtract, multiply, and divide

---
+++ calculator/calculations/division.py
@@ -0,0 +1,15 @@
+"""Division Class"""
+from calculator.calculations.calculation import Calculation
+
+class Division(Calculation):
+    """division calculation object"""
+    def get_result(self):
+        """get the division results"""
+        value_list = []
+
+        for value in self.values:
+            value_list.append(value)
+        result = value_list[0]
+        for num in range(1, len(list)):
+            result /= value_list[num]
+        return result
