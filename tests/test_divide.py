f3978f06ee0e2d5898100e2466bcd7fef4d09845
leo-ulloa <lfu3@njit.edu>
2021-11-09 20:46:28 -0500

    added calculations.py and test files

---
+++ tests/test_divide.py
@@ -0,0 +1,11 @@
+"""Testing Division"""
+from calculator.calculations.division import Division
+
+def test_calculation_subtraction():
+    """testing that our calculator has a static method for multiplication"""
+    #Arrange
+    nums = (8.0,2.0)
+    division = Division(nums)
+    #Act
+    #Assert
+    assert division.get_result() == 4.0
