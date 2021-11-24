63cea1ac9423fb3dfc42fae983890f5ce26428dd
leo-ulloa <lfu3@njit.edu>
2021-11-24 10:26:01 -0500

    Inheritance example for Project 2

--- tests/test_addition.py
+++
@@ -1,8 +0,0 @@
-"""Testing Addition"""
-from calculator.calculations.addition import Addition
-
-def test_calculation_addition():
-    """testing that our calculator has a static method for addition"""
-    nums = (3.0,2.0)
-    addition = Addition(nums)
-    assert addition.get_result() == 5.0
