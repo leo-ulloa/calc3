7f031e62494385b9186bc456bf673d3c40aea453
leo-ulloa <lfu3@njit.edu>
2021-11-24 10:32:31 -0500

    Polymorphism example for Project 2

--- tests/test_multiply.py
+++
@@ -1,8 +0,0 @@
-"""Testing Multiplication"""
-from calculator.calculations.multiplication import Multiplication
-
-def test_calculation_subtraction():
-    """testing that our calculator has a static method for multiplication"""
-    nums = (4.0,2.0)
-    multiplication = Multiplication(nums)
-    assert multiplication.get_result() == 8.0
--- tests/test_subtraction.py
+++
@@ -1,8 +0,0 @@
-"""Testing Subtraction"""
-from calculator.calculations.subtraction import Subtraction
-
-def test_calculation_subtraction():
-    """testing that our calculator has a static method for addition"""
-    nums = (1.0,2.0)
-    subtraction = Subtraction(nums)
-    assert subtraction.get_result() == -1.0
